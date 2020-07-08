165d9efc2966ff461ca07bf9032e7e2e665d53b6
TobiasBauerC <tobiasbauer115@gmail.com>
2020-07-07 22:07:45 -0400

    Added VBallTools for Garrett

---
+++ Assets/Scripts/Tools/VBallTools.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class VBallTools
+{
+    /// <summary>
+    /// Gets the cursor's position in world space
+    /// </summary>
+    /// <returns></returns>
+    public static Vector3 GetCursorPosition()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorPos.z = 0.0f;
+        return cursorPos;
+    }
+}
