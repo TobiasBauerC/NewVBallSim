a9a4d7984ed8a7a76d20765a4755bb65fbd7e68e
TobiasBauerC <tobiasbauer115@gmail.com>
2021-01-09 17:42:39 -0500

    + Scriptable Object script that can hold difficulty data + Data for Easy, Medium, and Hard difficulties

---
+++ Assets/Scripts/DifficultyData/DifficultyData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Difficulty", menuName = "DifficultData", order = 1)]
+public class DifficultyData : ScriptableObject
+{
+    public int player = 0;
+    public int ai = 0;
+}
