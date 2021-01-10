7f8cde1a6831945cd8c86ccb85a6671bf03e2093
TobiasBauerC <tobiasbauer115@gmail.com>
2021-01-10 18:34:34 -0500

    + Volume sliders/input fields to options menu + Saving volume between games + Loading screen that can be used to call all Init() functions before game loads

---
+++ Assets/Scripts/Menu/LoadingPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LoadingPanel : MonoBehaviour
+{
+    public UnityEvent eventsBeforeLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventsBeforeLoad.Invoke();
+        gameObject.SetActive(false);
+    }
+
+    public void EnableLoadingPanel()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Menu/SetVolume.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public string volumeParamName;
+    public AudioMixer mixer;
+    public Slider slider;
+    public InputField inputField;
+
+    public void Init()
+    {
+        SetLevel(PlayerPrefs.GetFloat(volumeParamName, 1.0f));
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat(volumeParamName, Mathf.Log10(sliderValue) * 20.0f);
+        inputField.text = Mathf.RoundToInt(sliderValue * 100).ToString();
+        slider.SetValueWithoutNotify(sliderValue);
+        PlayerPrefs.SetFloat(volumeParamName, sliderValue);
+    }
+
+    public void SetLevel(string inputFieldValue)
+    {
+        float newVol = Mathf.Clamp(float.Parse(inputFieldValue), 0, 100);
+        newVol = newVol / 100.0f;
+        newVol = Mathf.Clamp(newVol, 0.001f, 1.0f);
+        SetLevel(newVol);
+    }
+}
